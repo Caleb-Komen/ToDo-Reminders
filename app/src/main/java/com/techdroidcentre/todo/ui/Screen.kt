192fdaa6705ed83c875d01411ba743228ca176ac
Caleb-Komen <calebkipyatich@gmail.com>
2022-11-06 15:55:01 +0300

    Remove unused property.

--- app/src/main/java/com/techdroidcentre/todo/ui/Screen.kt
+++ app/src/main/java/com/techdroidcentre/todo/ui/Screen.kt
@@ -5,13 +5,13 @@ const val TASKS_SCREEN_ROUTE = "tasks"
 const val COLOUR_KEY = "colour"
 const val TODOLIST_ID_KEY = "todolist_id"
 
-sealed class Screen(val route: String, val title: String) {
-    object HomeScreen: Screen("$HOME_SCREEN_ROUTE?id={$TODOLIST_ID_KEY}&colour={$COLOUR_KEY}", "Home") {
+sealed class Screen(val route: String) {
+    object HomeScreen: Screen("$HOME_SCREEN_ROUTE?id={$TODOLIST_ID_KEY}&colour={$COLOUR_KEY}") {
         fun passIdAndColour(id: Long, colour: Int): String {
             return "$HOME_SCREEN_ROUTE?id=$id&colour=$colour"
         }
     }
-    object TasksScreen: Screen("$TASKS_SCREEN_ROUTE/{$TODOLIST_ID_KEY}/{$COLOUR_KEY}", "Tasks") {
+    object TasksScreen: Screen("$TASKS_SCREEN_ROUTE/{$TODOLIST_ID_KEY}/{$COLOUR_KEY}") {
         // helper method for passing arguments
         fun passTodoListIdAndColour(todoListId: Long, colour: Int): String {
             return "$TASKS_SCREEN_ROUTE/$todoListId/$colour"
